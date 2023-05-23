f3b7b7123511838b758f43444a755b858f1786be
JOYCE <joychinwenduibeh93@gmail.com>
2023-05-23 23:08:59 +0100

    added login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './index.css';
 import './App.css';
 import MainOtp from './Components/MainOtp';
+//import Login  from './Components/Login';
 
 function App() {
   return (
     <div>
       <MainOtp />
+      {/* <Login/> */}
+    
     </div>
   );
 }
