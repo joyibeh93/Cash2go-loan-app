7b403abec1e0e073f98366e71e0df2a6f027bcfe
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-05-25 23:24:52 +0100

    made few changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './index.css';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpStep2 from './Pages/SignUpStep2';
+import Dashboard from './Pages/Dashboard';
 import ResetPassword from './Pages/ResetPassword';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUpStep2 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signupstep2" element={<SignUpStep2 />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
       </Routes>
     </Router>
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard-container">
+      <div className="left-sidebar"></div>
+      <div className="main-dashboard"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
