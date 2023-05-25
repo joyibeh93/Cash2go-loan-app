808ffa387866869c3c45196e48b45ce147a00f3e
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-05-26 00:50:57 +0100

    deleted two files because of error

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpStep2 from './Pages/SignUpStep2';
 import ResetPassword from './Pages/ResetPassword';
-import ResetStep2 from './Pages/ResetStep2';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signupstep2" element={<SignUpStep2 />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
-        <Route path="/" element={<ResetStep2 />} />
       </Routes>
     </Router>
   );
