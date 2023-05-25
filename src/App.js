b66cf31770a316eb24723e37a0a680b82063a51b
Tina Namalima <tinan.tn1@gmail.com>
2023-05-25 14:20:57 +0200

    updated route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './index.css';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpStep2 from './Pages/SignUpStep2';
+import SignUpStep1 from './Components/SignUpStep1';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route original path="/" element={<SignUpStep1 />} />
         <Route path="/signupstep2" element={<SignUpStep2 />} />
       </Routes>
     </Router>
