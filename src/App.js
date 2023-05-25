2cb1b4ae1f0a216f1b732390b5ccb1bbd616c61b
Oluwatobi <116016360+Thobbie30@users.noreply.github.com>
2023-05-25 04:58:19 -0700

    changes made in app.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpStep2 from './Pages/SignUpStep2';
 import ResetPassword from './Pages/ResetPassword';
+import SignUpStep3page from './Pages/SignUpStep3page';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-
-        <Route path="/" element={<SignUpStep2 />} />
+        <Route path="/signupstep2" element={<SignUpStep2 />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
-
+        <Route path="/" element={<SignUpStep3page />} />
       </Routes>
     </Router>
   );
