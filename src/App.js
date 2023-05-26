1128f64b8599efe881ae2127e42c7f86ad36391e
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-05-26 15:28:47 +0100

    made some delete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import SignUpStep2 from './Pages/SignUpStep2';
 import ResetPassword from './Pages/ResetPassword';
 import SignUpStep4 from './Pages/SignUpStep4';
 import SignUpStep3 from './Pages/SignUpStep3page';
-import Login from './Pages/Login';
 function App() {
   return (
     <Router>
@@ -16,7 +15,6 @@ function App() {
         <Route path="/resetpassword" element={<ResetPassword />} />
         <Route path="/signupstep4" element={<SignUpStep4 />} />
         <Route path="/signupstep3" element={<SignUpStep3 />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
