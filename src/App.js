672de15a276dbe78dae1b1247cbb1bd5f7dfe1fa
JOYCE <joychinwenduibeh93@gmail.com>
2023-05-27 01:51:32 +0100

    did the linkimg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './index.css';
 import './App.css';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpStep2 from './Pages/SignUpStep2';
-// import Dashboard from './Pages/Dashboard';
+//import Dashboard from './Pages/Dashboard';
 import ResetPassword from './Pages/ResetPassword';
 import SignUpStep4 from './Pages/SignUpStep4';
 import SignUpStep3 from './Pages/SignUpStep3page';
+import Login from './Pages/Login'
 function App() {
   return (
+   
     <Router>
       <Routes>
         {/* <Route path="/" element={<Dashboard />} /> */}
@@ -15,6 +18,8 @@ function App() {
         <Route path="/resetpassword" element={<ResetPassword />} />
         <Route path="/signupstep4" element={<SignUpStep4 />} />
         <Route path="/signupstep3" element={<SignUpStep3 />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </Router>
   );
