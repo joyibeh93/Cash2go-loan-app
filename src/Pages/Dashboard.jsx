b75c11b14ec5b2307d11c59cc775a6902fc74584
JOYCE <joychinwenduibeh93@gmail.com>
2023-05-31 13:01:29 +0100

    added the changes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import Sidebar from '../Components/Sidebar';
+import '../dashboard.css'
+
 
 const Dashboard = () => {
   return (
+
     <div className="dashboard-container">
-      <div className="left-sidebar"></div>
+      <div className="left-sidebar">
+       <Sidebar/>
+      </div>
+      <div className='nav-bar'>
+        <Navbar/>
+      </div> 
       <div className="main-dashboard"></div>
     </div>
   );
