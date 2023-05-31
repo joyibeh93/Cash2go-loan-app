f5040de7ba7af0b6560001d44e7927ec751616c3
JOYCE <joychinwenduibeh93@gmail.com>
2023-05-31 12:58:49 +0100

    added the structure

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 const Dashboard = () => {
-<<<<<<< HEAD
-  return <div>Dashboard</div>;
-=======
   return (
     <div className="dashboard-container">
       <div className="left-sidebar"></div>
       <div className="main-dashboard"></div>
     </div>
   );
->>>>>>> dev1
 };
 
 export default Dashboard;
