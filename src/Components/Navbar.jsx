899db0feb7b30d632294f64e7e7631cff445c76d
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-06-02 15:51:42 +0100

    made changes to style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import usericon from '../assets/user-icon.svg'
-import searchicon from '../assets/Search.svg'
-import rightarrow from '../assets/Right-arrow.svg'
-import '../Styles/navbar.css'
-
+import React from 'react';
+import usericon from '../assets/user-icon.svg';
+import searchicon from '../assets/Search.svg';
+import rightarrow from '../assets/Right-arrow.svg';
+import '../Styles/navbar.css';
+import bellIcon from '../assets/Bell-iconont S';
 function Navbar() {
   return (
-    <div className='navbar-item'>
-      <img src={searchicon} alt="searhicon" className='searchicon' />
-      <input type="search" placeholder='Search' className='navbar-search'/>
-      <img src={rightarrow} alt="right-arrow" className='right-arrow' />
-      <span className='navbar-item2'>
-        <span>
-          <img src={usericon} alt="user-icon" className='navbar-userIcon' />
-        </span>
-        <span className='name-desc'>
+    <div className="navbar-item">
+      <img src={searchicon} alt="searhicon" className="searchicon" />
+      <input type="search" placeholder="Search" className="navbar-search" />
+      <img src={rightarrow} alt="right-arrow" className="right-arrow" />
+      <div className="navbar-item2">
+        <img src={bellIcon} alt="bell-icon" />
+        <div>
+          <img src={usericon} alt="user-icon" className="navbar-userIcon" />
+        </div>
+        <div className="name-desc">
           <p>Gbenger Stutern</p>
           <p> Loan Analytics</p>
-        </span>
-      </span>
-      
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
