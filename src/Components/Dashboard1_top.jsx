947392292a0a8379df4caa508d4349cd64027b4a
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-06-02 12:07:34 +0100

    styling the dashboard

--- src/Components/Dashboard1_top.jsx
+++ src/Components/Dashboard1_top.jsx
@@ -1,72 +1,72 @@
-import React from "react";
-import approvedTransparent from "../assets/approved-transparent.svg";
-import approvedBlack from "../assets/approved-transparent.svg";
-
+import React from 'react';
+import approvedTransparent from '../assets/approved-transparent.svg';
+import approvedBlack from '../assets/approved-transparent.svg';
+import '../Styles/Dashboard1-top.css';
 const Dashboard1_top = () => {
   return (
     <>
-    {/* 1st section */}
-    <div className="dashbord1-top">
-      <p>Link &gt Link </p>
-      <div className="container1">
-        <div className="welcomediv">
-          <h3>Hello Gbenga</h3>
-          <p>Welcome back to your all in dashboard</p>
+      {/* 1st section */}
+      <div className="dashbord1-top">
+        <p>Link &gt Link </p>
+        <div className="container-one">
+          <div className="welcomediv">
+            <h3>Hello Gbenga</h3>
+            <p>Welcome back to your all in dashboard</p>
+          </div>
+          <div className="buttondiv">
+            <button className="existing">Existing</button>
+            <button className="new">New</button>
+          </div>
         </div>
-        <div className="buttondiv">
-          <button className="existing">Existing</button>
-          <button className="new">New</button>
-        </div>
-      </div>
 
-      {/* 2nd section */}
-      <div className="container2">
-        <article className="status-bar1">
-          <div className="status">
-            <p>Approved Loans</p>
-            {approvedBlack}
-          </div>
-          <p className="statusNum">104</p>
-          <div className="status-bar2">
-            <p>+10 from yesterday</p>
-            <div className="labelDiv">
-              {approvedTransparent}
-              <p>Label</p>
+        {/* 2nd section */}
+        <div className="container2">
+          <article className="status-bar1">
+            <div className="status">
+              <p>Approved Loans</p>
+              {approvedBlack}
             </div>
-          </div>
-        </article>
-        <div className="linediv"></div>
-        <article className="status-bar">
-          <div className="status">
-            <p>Pending Reviews</p>
-            {approvedBlack}
-          </div>
-          <p className="statusNum">81</p>
-          <div className="status-bar2">
-            <p>+2 from yesterday</p>
-            <div className="labelDiv">
-              {approvedTransparent}
-              <p>Label</p>
+            <p className="statusNum">104</p>
+            <div className="status-bar2">
+              <p>+10 from yesterday</p>
+              <div className="labelDiv">
+                {approvedTransparent}
+                <p>Label</p>
+              </div>
             </div>
-          </div>
-        </article>
-        <div className="linediv"></div>
-        <article className="status-bar">
-          <div className="status">
-            <p>Rejected</p>
-            {approvedBlack}
-          </div>
-          <p className="statusNum">20</p>
-          <div className="status-bar2">
-            <p>+1 from yesterday</p>
-            <div className="labelDiv">
-              {approvedTransparent}
-              <p>Label</p>
+          </article>
+          <div className="linediv"></div>
+          <article className="status-bar">
+            <div className="status">
+              <p>Pending Reviews</p>
+              {approvedBlack}
             </div>
-          </div>
-        </article>
+            <p className="statusNum">81</p>
+            <div className="status-bar2">
+              <p>+2 from yesterday</p>
+              <div className="labelDiv">
+                {approvedTransparent}
+                <p>Label</p>
+              </div>
+            </div>
+          </article>
+          <div className="linediv"></div>
+          <article className="status-bar">
+            <div className="status">
+              <p>Rejected</p>
+              {approvedBlack}
+            </div>
+            <p className="statusNum">20</p>
+            <div className="status-bar2">
+              <p>+1 from yesterday</p>
+              <div className="labelDiv">
+                {approvedTransparent}
+                <p>Label</p>
+              </div>
+            </div>
+          </article>
+        </div>
       </div>
-    </div>
     </>
   );
 };
