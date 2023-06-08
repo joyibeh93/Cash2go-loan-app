f20b15fe7561715050b946a2143190e459efcd44
Oluwatobi <116016360+Thobbie30@users.noreply.github.com>
2023-06-07 19:41:52 -0700

    addition of upload bank statement

--- src/Components/Bvn.jsx
+++ src/Components/Bvn.jsx
@@ -28,9 +28,12 @@ const Bvn = () => {
 
   return (
     <div>
-      <h1 className="form-title">New Application</h1>
+      <h1 className="form-title">Applications</h1>
+      <p className="form-title">View All Loan application</p>
       <form className="form-container" onSubmit={handleSubmit}>
-        <p>Applicants BVN:</p>
+        <h3>Use BVN</h3>
+        <br></br>
+        <p>Applicants BVN</p>
         <input
           type="text"
           name="bvn"
@@ -42,6 +45,30 @@ const Bvn = () => {
           value={bvn}
           onChange={handleBvnChange}
         />
+        {bvnError && <p className="error-message">{bvnError}</p>}
+        <br/><br/>
+        <button type="submit" className="submit-button"> Next <span>&#8594;</span></button>
+      </form>
+
+   
+
+      <form className="form-container" onSubmit={handleSubmit}>
+        <h3>Upload Bank Statement</h3>
+        <br></br>
+        <p>Upload Bank Details</p>
+        <input
+          type="text"
+          name="bvn"
+          required
+          className="input-field"
+          placeholder="2380111111111111"
+          inputMode="numeric"
+          pattern="[0-9]*"
+          value={bvn}
+          onChange={handleBvnChange}
+        />
+        <p>Upload PDF. Check sample Here</p>
+
         {bvnError && <p className="error-message">{bvnError}</p>}
         <br/><br/>
         <button type="submit" className="submit-button"> Next <span>&#8594;</span></button>
