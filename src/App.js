9f8814af5188c8cd99e426e897fb5fd32c22f074
Jayetosin <Jayetosin>
2023-06-08 20:47:16 +0100

    charts

--- src/App.js
+++ src/App.js
@@ -7,30 +7,34 @@ import Dashboard from './Pages/Dashboard';
 import ResetPassword from './Pages/ResetPassword';
 import SignUpStep4 from './Pages/SignUpStep4';
 import SignUpStep3 from './Pages/SignUpStep3page';
-import Login from './Pages/Login';
+/* import Login from './Pages/Login'; */
 import PasswordResetPage from './Pages/PasswordResetPage';
 import PredictionInfoPage from './Pages/PredictionInfoPage';
 import ContactInfoPage from './Pages/ContactInfoPage';
 import ResetPassword3page from './Pages/ResetPassword3page';
-import BvnPage from './Pages/BvnPage';
+/* import BvnPage from './Pages/BvnPage';  */
+import ApplicantDetails from './Pages/ApplicantInfo';
+
 function App() {
   return (
     <Router>
       <Routes>
-         <Route path="/" element={<BvnPage />} /> 
+         {/*  <Route path="/" element={<BvnPage />} />  */}
         <Route path="/dashboard" element={<Dashboard />} /> 
         <Route path="/signupstep2" element={<SignUpStep2 />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
         <Route path="/signupstep4" element={<SignUpStep4 />} />
         <Route path="/signupstep3" element={<SignUpStep3 />} />
-        <Route exact path="/" element={<Login />} />
+        {/* <Route exact path="/" element={<Login />} /> */}
         <Route path="/signupstep1" element={<SignUpStep1 />} />
         <Route path="/resetpassword2" element={<PasswordResetPage />} />
         <Route path="/predictioninfo" element={<PredictionInfoPage />} />
         <Route path="/contactinfo" element={<ContactInfoPage />} />
-  <Route path="/resetpassword3" element={<ResetPassword3page />} />
+        <Route path="/resetpassword3" element={<ResetPassword3page />} />
+        <Route path="/" element={< ApplicantDetails/>} />
       </Routes>
     </Router>
+
       );
 }
 export default App;
