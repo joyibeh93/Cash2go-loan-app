f31f98ba591e309b6be6f82848f14fe2c53cc5f6
EbubeEvan <ebubemmayie@gmail.com>
2023-06-18 21:36:09 +0100

    complete messages

--- src/Components/MessageContent.jsx
+++ src/Components/MessageContent.jsx
@@ -3,61 +3,86 @@ import "../Styles/Inbox.css";
 import Inbox from "./Inbox";
 import MsgForm from "./MsgForm";
 import ThreadForm from "./ThreadForm";
+import Modal from "./Modal";
 
 function MessageContent() {
   const [message, setMessage] = useState("inbox");
   const [markAllClicked, setMarkAllClicked] = useState(false);
-
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleMarkAllClick = () => {
     setMarkAllClicked(!markAllClicked);
-    
   };
 
   return (
     <div className="messageContent">
+      {modalOpen && <Modal closeModal={setModalOpen} />}
       <div className="msg-top-bar">
         <div>
           <p className="msg-cnt-mb">
-            Message <span>&gt;</span> {
-              message === 'inbox' ? 'Inbox' : 'Create New'
-            }
+            Message <span>&gt;</span>{" "}
+            {message === "inbox" ? "Inbox" : "Create New"}
           </p>
           <h1>Messages</h1>
         </div>
-        { message === 'inbox' ? (
+        {message === "inbox" ? (
           <div className="exit-new">
-          <button className="markAll">Mark All</button>
-          {markAllClicked ? (
-            <button className="markRead">Delete Marked</button>
-          ) : (
-            <button className="markRead" onClick={handleMarkAllClick}>
-              Mark All as Read
-            </button>
-          )}
-        </div>
+            <button className="markAll">Mark All</button>
+            {markAllClicked ? (
+              <button className="markRead" onClick={() => setModalOpen(true)}>
+                Delete Marked
+              </button>
+            ) : (
+              <button className="markRead" onClick={handleMarkAllClick}>
+                Mark All as Read
+              </button>
+            )}
+          </div>
         ) : (
-          <p className="back-nav"><span className="back">&larr;</span>Back</p>
+          <p className="back-nav">
+            <span className="back">&larr;</span>Back
+          </p>
         )}
       </div>
       <div className="msg-nav">
         <div className="msg-section" onClick={() => setMessage("inbox")}>
           <p>Inbox</p>
-          <div className="msg-line" style={ message === 'inbox' ? {backgroundColor: '#747a74'} : {backgroundColor: '#bac0ba'}}></div>
+          <div
+            className="msg-line"
+            style={
+              message === "inbox"
+                ? { backgroundColor: "#747a74" }
+                : { backgroundColor: "#bac0ba" }
+            }
+          ></div>
         </div>
         <div className="msg-section" onClick={() => setMessage("new")}>
           <p>New</p>
-          <div className="msg-line" style={ message === 'new' ? {backgroundColor: '#747a74'} : {backgroundColor: '#bac0ba'}}></div>
+          <div
+            className="msg-line"
+            style={
+              message === "new"
+                ? { backgroundColor: "#747a74" }
+                : { backgroundColor: "#bac0ba" }
+            }
+          ></div>
         </div>
-        <div className="msg-section" >
+        <div className="msg-section">
           <p>Sent</p>
           <div className="msg-line"></div>
         </div>
         <div className="msg-section" onClick={() => setMessage("thread")}>
           <p>Thread</p>
-          <div className="msg-line" style={ message === 'thread' ? {backgroundColor: '#747a74'} : {backgroundColor: '#bac0ba'}}></div>
+          <div
+            className="msg-line add"
+            style={
+              message === "thread"
+                ? { backgroundColor: "#747a74" }
+                : { backgroundColor: "#bac0ba" }
+            }
+          ></div>
         </div>
-        <div className="msg-section" >
+        <div className="msg-section">
           <p>Trash</p>
           <div className="msg-line"></div>
         </div>
--- src/Components/MsgForm.jsx
+++ src/Components/MsgForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Styles/MsgForm.css";
+import UpAngle from "../assets/UpAngle.jpg";
 
 const MsgForm = () => {
   return (
@@ -20,20 +21,25 @@ const MsgForm = () => {
       <div className="linediv"></div>
       <div className="msg-main">
         <div className="msg-title">
-          <label htmlFor="message-title" className="msg-label">Message Title Here</label>
-          <input
-            type="text"
-            placeholder="Title"
-            className="msg-title-input"
-          />
-          <div className="ellipses2">...</div>
+          <div className="lab-el">
+            <label htmlFor="message-title" className="msg-label">
+              Message Title Here
+            </label>
+            <div className="ellipses2">...</div>
+          </div>
+          <input type="text" placeholder="Title" className="msg-title-input" />
         </div>
         <label htmlFor="body" className="body">
           Body
         </label>
-        <textarea cols="120" rows="8" placeholder="Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group.
+        <img src={UpAngle} alt="up-angle" className="up-angle" />
+        <textarea
+          cols="120"
+          rows="8"
+          placeholder="Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group.
 
-        Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group. Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group."></textarea>
+        Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group. Image asset list variant image. Mask layout underline invite mask effect shadow. Star bullet shadow overflow shadow. Content device rectangle project link create invite star group."
+        ></textarea>
         <div className="linediv"></div>
         <button className="send">Send</button>
       </div>
