aa3545f25965d8e3babce1cdaefd391165c1599f
JOYCE <joychinwenduibeh93@gmail.com>
2023-07-20 15:18:29 +0100

    working on filtering

---
+++ src/Components/datainfo.jsx
@@ -0,0 +1,13 @@
+// Example data, replace this with your actual data source
+const data = [
+    // ... Your data here ...
+    {
+      name: 'Ogbeni Malani',
+      date: '02 / 04 / 23',
+      status: 'Approved',
+      creditScore: 810,
+      amount: 'N 35,000.00',
+    },
+    // ... More data ...
+  ];
+  export default data
